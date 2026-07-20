44e57da44c45bd82b520362de7b7859c7321167f
bhalufavnum3-web <bhalufavnum3@gmail.com>
2026-07-20 12:30:02 +0530

    Add variables and print method to variable class

--- TNSIF_Training_java_B2/org/tnsif/acc/c2tc/javabasics/variable.java
+++ TNSIF_Training_java_B2/org/tnsif/acc/c2tc/javabasics/variable.java
@@ -1,10 +1,20 @@
 package org.tnsif.acc.c2tc.javabasics;
-
 public class variable {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	int price=10;
+	void print()
+	{
+		String msg ="hello";
+		System.out.println("msg");
+	}
+	static String message="hello students";
+	
+	public static void main(String [] args)
+	{
+		variable obj=new variable();
+		System.out.println(obj.price);
+		obj.print();
+		System.out.println(message);
+		
 	}
 
 }
