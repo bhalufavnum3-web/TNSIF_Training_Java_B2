686e9fe2ed2a1e53668f69b081fdd33344a9d3a6
chintu <chintu@BHARATH>
2026-07-21 18:27:23 +0530

    second commit

---
+++ TNSFI_Training/src/jr_developer/org/tnsif/acc/c2tc/KIA_Airbags.java
@@ -0,0 +1,16 @@
+package jr_developer.org.tnsif.acc.c2tc;
+
+public class KIA_Airbags
+{
+	public void disp_Airbags()
+	{
+		System.out.println("ALL KIA have Airbag 2");
+	}
+
+	public static void main(String[] args) {
+		KIA_Airbags bag=new KIA_Airbags();
+		bag.disp_Airbags();
+
+	}
+
+}
